5e63eeb724a70bdc92f9edb1cd3f48a6eedf9765
golub001 <aleksandarg03@gmail.com>
2025-10-19 17:25:54 +0200

    dodao sam api-gateway i dodao sam service usera sa entity

---
+++ user-service/src/main/java/Entity/User.java
@@ -0,0 +1,31 @@
+package Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity(name="user")
+@Table(name="users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String phone;
+    @Column(nullable = false)
+    private String location;
+}
